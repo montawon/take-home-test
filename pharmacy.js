ad6639f4dcccdbb1075ef17117b2ad6e355de530
tcollet <colletwebpro@gmail.com>
2021-11-01 17:49:54 +0100

    FIX: removed unecessary comments and dead code + added comment to functions

--- pharmacy.js
+++ pharmacy.js
@@ -11,18 +11,10 @@ export class Pharmacy {
     this.drugs = drugs;
   }
 
-  // Une fois la date d'expiration en dessous de zero, le benefit se dégrade 2x plus vite
-  // Expiration <= 0 : benefit = benefit - (2x pourri)
-  // Expiration > 0 : benefit = benefit - (1x pourri)
-
-  // if benefit <= 0 {benefit = 0}
-  // if benefit >= 50 {benefit = 50 }
-
-  // Magic Pill => Expiration speed = 0; pourri = 0
-  // Herbal Tea => pourri = -1
-  // Fervex => pourri = -1; If Expiration date <= 10 {pourri = -2}; if Expiration Date  <= 5  {pourri = -3}; If Expiration Date = 0 { pourri = benefit }
-  // Dafalgan => pourri = -2
-
+  /**
+   * Called to handle the action of time on the current drug and apply it
+   * @param {Drug} currentDrug 
+   */
   updateExpiration(currentDrug) {
     let expirationSpeed = 1;
 
@@ -34,7 +26,7 @@ export class Pharmacy {
    * As the Fervex drug has multiple efficiency changes over time we can use a specific function
    * to handle the logic and returning the accurate benefitEffect for this.updateDrugEfficiency()
    * @param {Drug} drug of the current drug
-   * @returns
+   * @returns benefitEffect
    */
   handleFervexCase(currentdrug) {
     const expirationDays = currentdrug.expiresIn
@@ -53,9 +45,14 @@ export class Pharmacy {
     }
   }
 
+  /**
+   * Considering time affects our products and some in specific ways, this function
+   * helps us handling this and getting a clear view on which are specific cases and
+   * how time affects the benefit by applying "benefitEffect" to it
+   * @param {Drug} currentDrug 
+   */
   updateDrugEfficiency(currentDrug) {
     let benefitEffect = currentDrug.expiresIn >= 0 ? 1 : 2;
-
     switch (currentDrug.name) {
       case "Dafalgan":
         benefitEffect = 2 * benefitEffect;
@@ -70,9 +67,14 @@ export class Pharmacy {
         benefitEffect = benefitEffect;
         break;
     }
+
     currentDrug.benefit = currentDrug.benefit - benefitEffect;
   }
 
+  /**
+   * Make sure no benefit data gets out of range by caping them to 0 (min) and 50 (max)
+   * @param {Drug} currentDrug 
+   */
   handlingBenefitCap(currentDrug) {
     if (currentDrug.benefit < 0) {
       currentDrug.benefit = 0;
@@ -81,6 +83,12 @@ export class Pharmacy {
     }
   }
 
+  /**
+   * updateBenefitValue loops through our drugs and call the different methods to 
+   * apply both time and its effect on the drugs we sell and returns the complete array
+   * as results
+   * @returns drugs array
+   */
   updateBenefitValue() {
     for (var i = 0; i < this.drugs.length; i++) {
       // Note: Magic pill does not change over time, it's a very specific case
@@ -91,54 +99,6 @@ export class Pharmacy {
         this.updateDrugEfficiency(this.drugs[i]);
         this.handlingBenefitCap(this.drugs[i]);
       }
-
-      // if (
-      //   this.drugs[i].name != "Herbal Tea" &&
-      //   this.drugs[i].name != "Fervex"
-      // ) {
-      //   if (this.drugs[i].benefit > 0) {
-      //     if (this.drugs[i].name != "Magic Pill") {
-      //       this.drugs[i].benefit = this.drugs[i].benefit - 1;
-      //     }
-      //   }
-      // } else {
-      //   if (this.drugs[i].benefit < 50) {
-      //     this.drugs[i].benefit = this.drugs[i].benefit + 1;
-      //     if (this.drugs[i].name == "Fervex") {
-      //       if (this.drugs[i].expiresIn < 11) {
-      //         if (this.drugs[i].benefit < 50) {
-      //           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-      //         }
-      //       }
-      //       if (this.drugs[i].expiresIn < 6) {
-      //         if (this.drugs[i].benefit < 50) {
-      //           this.drugs[i].benefit = this.drugs[i].benefit + 1;
-      //         }
-      //       }
-      //     }
-      //   }
-      // }
-      // if (this.drugs[i].name != "Magic Pill") {
-      //   this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
-      // }
-      // if (this.drugs[i].expiresIn < 0) {
-      //   if (this.drugs[i].name != "Herbal Tea") {
-      //     if (this.drugs[i].name != "Fervex") {
-      //       if (this.drugs[i].benefit > 0) {
-      //         if (this.drugs[i].name != "Magic Pill") {
-      //           this.drugs[i].benefit = this.drugs[i].benefit - 1;
-      //         }
-      //       }
-      //     } else {
-      //       this.drugs[i].benefit =
-      //         this.drugs[i].benefit - this.drugs[i].benefit;
-      //     }
-      //   } else {
-      //     if (this.drugs[i].benefit < 50) {
-      //       this.drugs[i].benefit = this.drugs[i].benefit + 1;
-      //     }
-      //   }
-      // }
     }
 
     return this.drugs;
